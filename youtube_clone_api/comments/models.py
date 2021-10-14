10ead19839c8cf28c38798c462306792c1dac014
JmA515 <janderson515@yahoo.com>
2021-10-14 08:39:12 -0700

    Paired programming with Garrett. Created Comment and Reply models. Added Cors settings.

--- youtube_clone_api/comments/models.py
+++ youtube_clone_api/comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    body = models.CharField(max_length=200)
+    videoId = models.CharField(max_length=50)
+    likes = models.IntegerField(max_length=10)
+    dislikes = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return self.body
+
+
+class Reply(models.Model):
+    body = models.CharField(max_length=200)
+    commentId = models.ForeignKey(Comment)
--- youtube_clone_api/youtube_clone_api/settings.py
+++ youtube_clone_api/youtube_clone_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'comments',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'youtube_clone_api.urls'
 
