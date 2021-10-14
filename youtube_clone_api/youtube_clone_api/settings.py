a309ef71feecfd891ac75dfb0fc91a465b7f669e
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-10-14 12:12:06 -0500

    Pair programmed with JP. Installed django cors headers. Installed autopep8 for shortcut assistance. Registered our models within the admin pytho python file. We removed the integerfield property from our models as it isn't needed. We made initial migrations to our database.

--- youtube_clone_api/youtube_clone_api/settings.py
+++ youtube_clone_api/youtube_clone_api/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+xl-il5fts8d95hpf0#^$r_*qv33y*(3eq#q5v8-hw2+=n6qxv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,22 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'youtube_clone_api.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'youtube_clone_api',
-        'USER': 'root',
-        'PASSWORD': 'Password1!',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'autocommit': True
-        }
-    }
-}
 
 
 # Password validation
@@ -135,3 +117,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from youtube_clone_api.local_settings import *
+except ImportError:
+    pass
